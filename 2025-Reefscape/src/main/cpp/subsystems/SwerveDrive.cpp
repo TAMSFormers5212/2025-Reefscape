1ef240ca0dace33bb0dce3253eb488ca0c9e8947
lonaslee <93890846+lonaslee@users.noreply.github.com>
2025-03-21 23:54:48 -0500

    ll

--- 2025-Reefscape/src/main/cpp/subsystems/SwerveDrive.cpp
+++ 2025-Reefscape/src/main/cpp/subsystems/SwerveDrive.cpp
@@ -201,8 +201,6 @@ void SwerveDrive::swerveDrive(frc::ChassisSpeeds speeds) {  // swerve drive
     }
 }
 
-
-
 void SwerveDrive::brake() {  // sets wheels to o position
     swerveDrive(0, 0, 0.05, false);
 }
@@ -277,40 +275,37 @@ void SwerveDrive::UpdatePoseEstimate() {
     m_odometry.Update(getGyroHeading2(),
                       {m_modules[0].getPosition(), m_modules[1].getPosition(),
                        m_modules[2].getPosition(), m_modules[3].getPosition()});
-    if (align) {
-        bool doRejectUpdate = false;
+    // LimelightHelpers::SetRobotOrientation(
+    //     "limelight",
+    //     m_odometry.GetEstimatedPosition().Rotation().Degrees().value(),
+    //     0, 0, 0, 0, 0);
 
-        LimelightHelpers::SetRobotOrientation(
-            "limelight",
-            getGyroHeading2().Degrees().value(), 0,
-            0, 0, 0, 0);
-        LimelightHelpers::PoseEstimate mt2 =
-            LimelightHelpers::getBotPoseEstimate_wpiBlue_MegaTag2("limelight");
-        auto pose = mt2.pose;
-        // m_odometry.UpdateWithTime(mt2.timestampSeconds, getGyroHeading2(),
-        //               {m_modules[0].getPosition(), m_modules[1].getPosition(),
-        //                m_modules[2].getPosition(), m_modules[3].getPosition()});
+    // LimelightHelpers::PoseEstimate mt2 =
+        // LimelightHelpers::getBotPoseEstimate_wpiBlue_MegaTag2("limelight");
+    LimelightHelpers::PoseEstimate mt1 =
+        LimelightHelpers::getBotPoseEstimate_wpiBlue("limelight");
 
-        frc::SmartDashboard::PutNumber("limelight x", pose.X().value());
-        frc::SmartDashboard::PutNumber("limelight y", pose.Y().value());
-        frc::SmartDashboard::PutNumber("limelight h", pose.Rotation().Degrees().value());
+    auto pose = mt1.pose;
 
-        if (abs(m_gyro.GetRate()) >
-            720)  // if our angular velocity is greater than 720 degrees per
-                  // second, ignore vision updates
-        {
-            doRejectUpdate = true;
-        }
-        if (mt2.tagCount == 0) {
-            doRejectUpdate = true;
-        }
-        if (!doRejectUpdate) {
-            wpi::array<double, 3U> temp = {.7, .7, 9999999};
-            // m_poseEstimator.SetVisionMeasurementStdDevs(VecBuilder.fill(.7,.7,9999999));
-            m_odometry.SetVisionMeasurementStdDevs(temp);
-            m_odometry.AddVisionMeasurement(mt2.pose, mt2.timestampSeconds);
+    frc::SmartDashboard::PutNumber("limelight x", pose.X().value());
+    frc::SmartDashboard::PutNumber("limelight y", pose.Y().value());
+    frc::SmartDashboard::PutNumber("limelight h",
+                                   pose.Rotation().Degrees().value());
 
-        }
+    bool shouldCorrect = true;
+    // if our angular velocity is greater than 720 degrees per
+    // second, ignore vision updates
+    if (abs(m_gyro.GetRate()) > 720) {
+        shouldCorrect = false;
+    }
+    if (mt1.tagCount == 0) {
+        shouldCorrect = false;
+    }
+    if (shouldCorrect && align) {
+        wpi::array<double, 3U> temp = {.5, .5, 9999999};
+        // m_poseEstimator.SetVisionMeasurementStdDevs(VecBuilder.fill(.7,.7,9999999));
+        m_odometry.SetVisionMeasurementStdDevs(temp);
+        m_odometry.AddVisionMeasurement(mt1.pose, mt1.timestampSeconds);
     }
 }
 
@@ -331,12 +326,6 @@ void SwerveDrive::Periodic() {
                                    pose.Translation().Y().value());
     frc::SmartDashboard::PutNumber("odometry rot",
                                    pose.Rotation().Degrees().value());
-    // frc::SmartDashboard::PutNumber("odometry x",
-    // m_odometry.GetPose().Translation().X().value());
-    // frc::SmartDashboard::PutNumber("odometry y",
-    // m_odometry.GetPose().Translation().Y().value());
-    // frc::SmartDashboard::PutNumber("odometry rot",
-    // m_odometry.GetPose().Rotation().Degrees().value());
 
     frc::SmartDashboard::PutNumber(
         "wheel pos",
@@ -386,17 +375,15 @@ frc::Rotation2d SwerveDrive::getVelocityHeading() {
 }
 
 frc::Pose2d SwerveDrive::getTargetPose() {
-    return frc::Pose2d(
-        OdometryPose().Translation().X() + units::meter_t{1},
-        OdometryPose().Translation().Y(),
-        OdometryPose().Rotation().Degrees());
+    return frc::Pose2d(OdometryPose().Translation().X() + units::meter_t{1},
+                       OdometryPose().Translation().Y(),
+                       OdometryPose().Rotation().Degrees());
 }
 
 frc2::CommandPtr SwerveDrive::driveToTargetPose(frc::Pose2d waypoint) {
     frc::ChassisSpeeds speeds = getFieldRelativeSpeeds();
     std::vector<frc::Pose2d> poses{
-        frc::Pose2d(OdometryPose().Translation(),
-                    getVelocityHeading()),
+        frc::Pose2d(OdometryPose().Translation(), getVelocityHeading()),
         waypoint};
 
     std::vector<Waypoint> waypoints =
@@ -408,8 +395,7 @@ frc2::CommandPtr SwerveDrive::driveToTargetPose(frc::Pose2d waypoint) {
     // units::meters_per_second_t thing{
     //     frc::Translation2d(speeds.vx, speeds.vy).Norm().value()};
     auto path = std::make_shared<PathPlannerPath>(
-        waypoints, constraints,
-        IdealStartingState(0.0_mps, getGyroHeading2()),
+        waypoints, constraints, IdealStartingState(0.0_mps, getGyroHeading2()),
         GoalEndState(0.0_mps,
                      waypoint.Rotation())  // Goal end state. You can set a
                                            // holonomic rotation here. If using
@@ -417,8 +403,9 @@ frc2::CommandPtr SwerveDrive::driveToTargetPose(frc::Pose2d waypoint) {
                                            // rotation will have no effect.
     );
     path->preventFlipping = true;
-    
-    // return AutoBuilder::followPath(path).AndThen([this] { alignAdjustment(); });
+
+    // return AutoBuilder::followPath(path).AndThen([this] { alignAdjustment();
+    // });
     return AutoBuilder::followPath(path);
 }
 
@@ -430,21 +417,17 @@ void SwerveDrive::alignAdjustment() {
     PPHolonomicDriveController ctrler = PPHolonomicDriveController(
         PIDConstants(5.0, 0.0, 0.0), PIDConstants(1.0, 0.0, 0.0));
 
-    swerveDrive(ctrler.calculateRobotRelativeSpeeds(
-        OdometryPose(), goalState));
+    swerveDrive(ctrler.calculateRobotRelativeSpeeds(OdometryPose(), goalState));
 }
 
 frc2::CommandPtr SwerveDrive::generateCommand() {
     // std::function<frc2::CommandPtr()> thing = [this] {
     //     return driveToTargetPose(getTargetPose());
     // };
-    std::initializer_list<frc2::Subsystem*> requirements = {this};
+    std::initializer_list<frc2::Subsystem *> requirements = {this};
     // return frc2::cmd::Defer([this] {
-        return driveToTargetPose(getTargetPose());
+    return driveToTargetPose(getTargetPose());
     // }, requirements);
     // return frc2::cmd::Defer(thing, frc2::Requirements(requirements));
     // return driveToTargetPose(getTargetPose());
 }
-
-
-
