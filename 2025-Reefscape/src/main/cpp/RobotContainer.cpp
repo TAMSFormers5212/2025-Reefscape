926b13512a91d42dc648193510e7394bb879a4d9
lonaslee <93890846+lonaslee@users.noreply.github.com>
2025-04-19 01:12:41 -0500

    stuf

--- 2025-Reefscape/src/main/cpp/RobotContainer.cpp
+++ 2025-Reefscape/src/main/cpp/RobotContainer.cpp
@@ -376,10 +376,9 @@ RobotContainer::RobotContainer() {
             }
             if (m_operatorController.GetRawAxis(Controller::rightTrigger) >
                 0.05) {
-                m_superstructure.m_intake.setSpeed(0.8);
-            } else if (m_operatorController.GetRawAxis(
-                           Controller::leftTrigger) > 0.05) {
-                m_superstructure.m_intake.setSpeed(-0.4);
+                m_superstructure.m_intake.setSpeed(m_operatorController.GetRawAxis(Controller::rightTrigger));
+            } else if (m_operatorController.GetRawAxis(Controller::leftTrigger) > 0.05) {
+                m_superstructure.m_intake.setSpeed(-m_operatorController.GetRawAxis(Controller::leftTrigger));
             } else /*(m_operatorController.GetRawAxis(Controller::leftTrigger) <
                      0.05 &&
                  m_operatorController.GetRawAxis(Controller::rightTrigger) <
@@ -390,11 +389,11 @@ RobotContainer::RobotContainer() {
             // m_superstructure.m_intake.setSpeed(
             //     m_operatorController.GetRawAxis(Controller::rightYAxis) / 6);
 
-            // if (m_driverController.GetRawButtonPressed(Controller::A)) {
-            //     m_superstructure.m_intake.groundPreset();
-            // } else if (m_driverController.GetRawButtonPressed(Controller::X)) {
-            //     m_superstructure.m_intake.stowPreset();
-            // }
+            if (m_driverController.GetRawButtonPressed(Controller::A)) {
+                m_superstructure.m_intake.groundPreset();
+            } else if (m_driverController.GetRawButtonPressed(Controller::X)) {
+                m_superstructure.m_intake.stowPreset();
+            }
         },
         {&m_superstructure.m_intake}));
 
--- 2025-Reefscape/src/main/cpp/subsystems/Intake.cpp
+++ 2025-Reefscape/src/main/cpp/subsystems/Intake.cpp
@@ -35,21 +35,21 @@ Intake::Intake(int intakeMotor, int pivotMotor, int encoder,
 void Intake::resetMotor() {
     m_intakeConfig.SetIdleMode(rev::spark::SparkBaseConfig::IdleMode::kBrake)
         .VoltageCompensation(12.0)
-        .SmartCurrentLimit(20, 25)
+        .SmartCurrentLimit(30, 30)
         .Inverted(true);
 
     m_intakeConfig.encoder.PositionConversionFactor(pi2 / intakeRatio);
 
     m_pivotConfig.SetIdleMode(rev::spark::SparkBaseConfig::IdleMode::kBrake)
         .VoltageCompensation(12.0)
-        .SmartCurrentLimit(20, 25)
+        .SmartCurrentLimit(30, 30)
         .Inverted(false);
 
     m_pivotConfig.encoder.PositionConversionFactor(pi2 / pivotRatio);
 
-    m_pivotConfig.closedLoop.Pidf(kiP, kiI, kiD, kiFF)
-        .IZone(kiIz)
-        .OutputRange(kiMinOutput, kiMaxOutput);
+    // m_pivotConfig.closedLoop.Pidf(kiP, kiI, kiD, kiFF)
+        // .IZone(kiIz)
+        // .OutputRange(kiMinOutput, kiMaxOutput);
 
     m_pivotEncoder.SetPosition(getPosition());
 
@@ -99,8 +99,8 @@ double Intake::getSpeed() { return m_encoder.GetVelocity(); }
 double Intake::getOutputCurrent() { return m_intakeMotor.GetOutputCurrent(); }
 
 void Intake::Periodic() {
-    const double kP = 0.002;
-    const double kG = 0.2;
+    const double kP = 0.02;
+    const double kG = 0.4;
     double currentPos;
 
     if (m_absoluteEncoder.IsConnected()) {
@@ -115,7 +115,7 @@ void Intake::Periodic() {
     double pid = error * kP;
     double ff = cos(currentPos / 180 * 3.14) * kG;
 
-    double power = ff;
+    double power = pid + ff;
     setPivotSpeed(power);
 
     // units::radian_t ffP{position*pi2/180};
@@ -127,6 +127,9 @@ void Intake::Periodic() {
                               m_absoluteEncoder.Get() - pivotOffset);
     SmartDashboard::PutNumber("intake abs pos", m_absoluteEncoder.Get());
     SmartDashboard::PutNumber("Intake neo pos", m_pivotEncoder.GetPosition());
+
+    SmartDashboard::PutNumber("intake motor current", m_intakeMotor.GetOutputCurrent());
+    SmartDashboard::PutNumber("pivot motor current", m_pivotMotor.GetOutputCurrent());
 
     SmartDashboard::PutNumber("pivot target pos", targetPos);
     SmartDashboard::PutNumber("pivot current pos", currentPos);
